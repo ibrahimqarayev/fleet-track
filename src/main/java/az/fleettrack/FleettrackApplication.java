5923893a98c38d67658fd2751dac7b948c600a5a
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-08-28 22:54:53 +0400

    chore: initialize fleet-track project

---
+++ src/main/java/az/fleettrack/FleettrackApplication.java
@@ -0,0 +1,13 @@
+package az.fleettrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleettrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleettrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/fleettrack/FleettrackApplicationTests.java
@@ -0,0 +1,13 @@
+package az.fleettrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleettrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
