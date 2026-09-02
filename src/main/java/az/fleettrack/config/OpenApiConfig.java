4cacb424c01cc1518e085cdde90d972a9de3e069
ibrahimqarayev <qarayevibrahim7@mail.ru>
2026-09-02 14:37:06 +0400

    feat: add OpenAPI Swagger UI documentation configuration and security schemes

---
+++ src/main/java/az/fleettrack/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package az.fleettrack.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI fleetTrackOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("FleetTrack API")
+                        .description("Fleet management and vehicle tracking REST API")
+                        .version("1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                "bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ));
+    }
+}
